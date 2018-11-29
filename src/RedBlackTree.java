1acf1ca32cc3aad6355acb7e25f85c8ade9f06c4
Sijia Gao <sijiagaowork@gmail.com>
2018-11-29 11:43:07 -0800

    debug

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -73,34 +73,23 @@ public class RedBlackTree<Key extends Comparable<Key>> {
         if (n == null) {
             return;
         }
-        v.visit(n);
+//        v.visit(n);
+        System.out.println(n.key + n.color);
+        //
+        if (n.leftChild != null) {
+//            System.out.println(n.leftChild.key);
+        }
+        if (n.rightChild != null){
+//            System.out.println(n.rightChild.key);
+        }
+        //
         preOrderVisit(n.leftChild, v);
         preOrderVisit(n.rightChild, v);
-
-    }
-
-    public void printTree(){  //preorder: visit, go left, go right
-        RedBlackTree.Node<String> currentNode = root;
-        printTree(currentNode);
-    }
-
-    /**
-     * Start at the root node and traverse the tree using preorder
-     * @param node
-     */
-    public void printTree(RedBlackTree.Node<String> node){
-        if (node != null) {
-            System.out.print(node.key);
-        }
-        if (isLeaf(node)){
-            return;
-        }
-        printTree(node.leftChild);
-        printTree(node.rightChild);
     }
 
     public void insert(String data) {
         addNode(root, data);
+//        insert(root, data);
     }
 
     /**
@@ -111,8 +100,10 @@ public class RedBlackTree<Key extends Comparable<Key>> {
         Node temp = null;
         // give the data a node
         Node insertNode = new Node(data);
-//        String keyTemp;
-//        String sibKey;
+        //
+        String keyTemp;
+        //
+        String sibKey;
 
         // at the first time, root passed into node, if the root is null
         if (node == null){
@@ -130,13 +121,16 @@ public class RedBlackTree<Key extends Comparable<Key>> {
                 }
             }
             insertNode.parent = temp;
-//            keyTemp = (String) temp.key;
+            //
+            keyTemp = (String) temp.key;
+            System.out.println(keyTemp);
 
             //debug ---
             if (getSibling(temp) != null){
                 Node sib = getSibling(temp);
-//                sibKey = (String) sib.key;
-//                System.out.println(sibKey);
+                //
+                sibKey = (String) sib.key;
+                System.out.println(sibKey);
             }
             // ----
             if (temp.key.compareTo(data) < 0) {
@@ -151,31 +145,133 @@ public class RedBlackTree<Key extends Comparable<Key>> {
         fixTree(insertNode);
     }
 
-    /**
-     * searches for a key.
-     * @param k
-     * @return
-     */
-    public boolean lookup(String k){
-        //fill
-        return lookup(root, k);
-    }
+//    private Node insert(Node node, String data) {
+//        Node temp = null;
+//        Node insertNode = new Node(data);
+//        while (node != null) {
+//            temp = node;
+//            if (node.key.compareTo(data) > 0)
+//                node = node.leftChild;
+//            else
+//                node = node.rightChild;
+//        }
+//        insertNode.parent = temp;
+//        if (temp == null) {
+//            root = insertNode;
+//            insertNode.color = 1;//black root
+//            return root;
+//        } else {
+//            if (insertNode.key.compareTo(data) < 0)
+//                temp.leftChild = insertNode;
+//            else
+//                temp.rightChild = insertNode;
+//        }
+//        insertNode.leftChild = null;
+//        insertNode.rightChild = null;
+//        insertNode.color = 0;
+//        fixTree(insertNode);
+//        return root;
+//
+//    }
 
-    public boolean lookup(Node node, String k){
-        //fill
-        boolean found = false;
-        while (node != null && !found){
-            // if the node's value is less than the look up value "k", then go left
-            if (node.key.compareTo(k) > 0){
-                node = node.leftChild;
-            } else if (node.key.compareTo(k) < 0){
-                node = node.rightChild;
-            } else {
-                found = true;
-                break;
+    /**
+     * recursive function: recursively traverse the tree to make it a Red Black tree.
+     * @param current
+     */
+    public void fixTree(RedBlackTree.Node<String> current) {
+        // debug
+        String keyValue = current.key;
+
+        Node parent = null;
+        String keyP;
+        Node grandParent = null;
+        String keyGP;
+        Node aunt = null;
+        String auntK;
+
+        // not sure if it is the original parent, or if it is necessary
+        if (getParent(current) != null){
+            parent = getParent(current);
+            keyP = getParent(current).key;
+//            System.out.println(keyP);
+        }
+        if (getGrandparent(current) != null) {
+            grandParent = getGrandparent(current);
+//            keyGP = getGrandparent(current).key;
+//            System.out.println(keyGP);
+        }
+        if (getAunt(current) != null) {
+            aunt = getAunt(current);
+//            auntK = getAunt(current).key;
+//            System.out.println(auntK);
+        }
+
+        // 1) current is the root node. Make it black and quit.
+        if (current.parent == null){
+            current.color = 1;
+            return;
+        }
+
+        // 2) Parent is black. Quit, the tree is a Red Black Tree.
+        if (parent.color == 1){
+            return;
+        }
+
+        //3) The current node is red and the parent node is red.
+        // The tree is unbalanced and you will have to modify it in the following way.
+        if (current.color == 0 && parent.color == 0){
+
+            // I. If the aunt node is empty or black, then there are four sub cases that you have to process.
+            if (getAunt(current) == null || getAunt(current).color == 1){
+
+                // A) grandparent ?parent(is left child)? current (is right child) case.
+                if (isLeftChild(grandParent, parent) && isRightChild(parent, current)){
+                    // Solution: rotate the parent left and then
+                    // continue recursively fixing the tree starting with the original parent.
+                    rotateLeft(parent);
+                    fixTree(parent);
+                }
+
+                // B) grandparent ?parent (is right child)? current (is left child) case.
+                if (isRightChild(grandParent, parent) && isLeftChild(parent, current)){
+                    // Solution: rotate the parent right and then
+                    // continue recursively fixing the tree starting with the original parent.
+                    rotateRight(parent);
+                    fixTree(parent);
+                }
+
+                //C) grandparent ?parent (is left child)? current (is left child) case.
+                if (isLeftChild(grandParent, parent) && isLeftChild(parent, current)){
+                    // Solution: make the parent black, make the grandparent red,
+                    // rotate the grandparent to the right and quit, tree is balanced
+                    parent.color = 1;
+                    grandParent.color = 0;
+                    rotateRight(grandParent);
+                    return;
+                }
+
+                // D) grandparent ?parent (is right child)? current (is right child) case.
+                if (isRightChild(grandParent, parent) && isRightChild(parent, current)){
+                    //Solution: make the parent black, make the grandparent red,
+                    // rotate the grandparent to the left, quit tree is balanced.
+                    parent.color = 1;
+                    grandParent.color = 0;
+                    rotateLeft(grandParent);
+                    return;
+                }
+            }
+
+            // II. Else if the aunt is red, then make the parent black,
+            // make the aunt black, make the grandparent red
+            // and continue recursively fix up the tree starting with the grandparent.
+            else if (aunt.color == 0){
+                parent.color = 1;
+                aunt.color = 1;
+                grandParent.color = 0;
+                fixTree(grandParent);
             }
         }
-        return found;
+
     }
 
     /**
@@ -279,110 +375,50 @@ public class RedBlackTree<Key extends Comparable<Key>> {
     }
 
     /**
-     * recursive function: recursively traverse the tree to make it a Red Black tree.
-     * @param current
+     * searches for a key.
+     * @param k
+     * @return
      */
-    public void fixTree(RedBlackTree.Node<String> current) {
-        // debug
-        String keyValue = current.key;
+    public boolean lookup(String k){
+        //fill
+        return lookup(root, k);
+    }
 
-        Node originalParent = null;
-        String keyP;
-        Node parent = null;
-        Node grandParent = null;
-        String keyGP;
-        Node aunt = null;
-        String auntK;
-
-        // not sure if it is the original parent, or if it is necessary
-        if (getParent(current) != null){
-            originalParent = getParent(current);
-            keyP = getParent(current).key;
-            // ---
-//            System.out.println(keyP);
-            parent = current.parent;
-//            String p = current.parent.key;
-            // ---
-//            System.out.println(p);
-        }
-        if (getGrandparent(current) != null) {
-            grandParent = getGrandparent(current);
-//            keyGP = getGrandparent(current).key;
-            // ---
-//            System.out.println(keyGP);
-        }
-        if (getAunt(current) != null) {
-            aunt = getAunt(current);
-            //---
-//            auntK = getAunt(current).key;
-//            System.out.println(auntK);
-        }
-
-
-        // 1) current is the root node. Make it black and quit.
-        if (current.parent == null){
-            current.color = 1;
-            return;
-        }
-
-        // 2) Parent is black. Quit, the tree is a Red Black Tree.
-        if (parent.color == 1){
-            return;
-        }
-
-        //3) The current node is red and the parent node is red.
-        // The tree is unbalanced and you will have to modify it in the following way.
-        if (current.color == 0 && parent.color == 0){
-
-            // I. If the aunt node is empty or black, then there are four sub cases that you have to process.
-            if (getAunt(current) == null || getAunt(current).color == 1){
-
-                // A) grandparent ?parent(is left child)? current (is right child) case.
-                if (isLeftChild(grandParent, parent) && isRightChild(parent, current)){
-                    // Solution: rotate the parent left and then continue recursively fixing the tree starting with the original parent.
-                    rotateLeft(parent);
-                    fixTree(originalParent);
-                }
-
-                // B) grandparent ?parent (is right child)? current (is left child) case.
-                if (isRightChild(grandParent, parent) && isLeftChild(parent, current)){
-                    // Solution: rotate the parent right and then continue recursively fixing the tree starting with the original parent.
-                    rotateRight(parent);
-                    fixTree(originalParent);
-                }
-
-                //C) grandparent ?parent (is left child)? current (is left child) case.
-                if (isLeftChild(grandParent, parent) && isLeftChild(parent, current)){
-                    // Solution: make the parent black, make the grandparent red,
-                    // rotate the grandparent to the right and quit, tree is balanced
-                    parent.color = 1;
-                    grandParent.color = 0;
-                    rotateRight(grandParent);
-                    return;
-                }
-
-                // D) grandparent ?parent (is right child)? current (is right child) case.
-                if (isRightChild(grandParent, parent) && isRightChild(parent, current)){
-                    //Solution: make the parent black, make the grandparent red,
-                    // rotate the grandparent to the left, quit tree is balanced.
-                    parent.color = 1;
-                    grandParent.color = 0;
-                    rotateLeft(grandParent);
-                    return;
-                }
-            }
-
-            // II. Else if the aunt is red, then make the parent black,
-            // make the aunt black, make the grandparent red
-            // and continue recursively fix up the tree starting with the grandparent.
-            else if (aunt.color == 0){
-                parent.color = 1;
-                aunt.color = 1;
-                grandParent.color = 0;
-                fixTree(grandParent);
+    public boolean lookup(Node node, String k){
+        //fill
+        boolean found = false;
+        while (node != null && !found){
+            // if the node's value is less than the look up value "k", then go left
+            if (node.key.compareTo(k) > 0){
+                node = node.leftChild;
+            } else if (node.key.compareTo(k) < 0){
+                node = node.rightChild;
+            } else {
+                found = true;
+                break;
             }
         }
+        return found;
+    }
 
+    public void printTree(){  //preorder: visit, go left, go right
+        RedBlackTree.Node<String> currentNode = root;
+        printTree(currentNode);
+    }
+
+    /**
+     * Start at the root node and traverse the tree using preorder
+     * @param node
+     */
+    public void printTree(RedBlackTree.Node<String> node){
+        if (node != null) {
+            System.out.print(node.key);
+        }
+        if (isLeaf(node)){
+            return;
+        }
+        printTree(node.leftChild);
+        printTree(node.rightChild);
     }
 
     /**
--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import java.awt.*;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 import static java.lang.System.currentTimeMillis;
 import static java.lang.System.setOut;
@@ -9,100 +14,96 @@ import static org.junit.Assert.assertEquals;
 
 public class Tester {
     public static void main(String[] args) {
+        RedBlackTree rbt = new RedBlackTree();
+        rbt.insert("D");
+        rbt.insert("B");
+        rbt.insert("A");
+        rbt.insert("C");
+        rbt.insert("F");
+        rbt.insert("E");
+        rbt.insert("H");
+        rbt.insert("G");
+        rbt.insert("I");
+        rbt.insert("J");
+//        rbt.insert("K");
 
-//        RedBlackTree rbt = new RedBlackTree();
-//        rbt.insert("D");
-//        rbt.insert("B");
-//        rbt.insert("A");
-//        rbt.insert("C");
-//        rbt.insert("F");
-//        rbt.insert("E");
-//        rbt.insert("H");
-//        rbt.insert("G");
-//        rbt.insert("I");
-//        rbt.insert("J");
-//
-//        System.out.println(makeString(rbt));
-//
-//        String str = "Color: 1, Key:D Parent: \n" +
-//                "Color: 1, Key:B Parent: D\n" +
-//                "Color: 1, Key:A Parent: B\n" +
-//                "Color: 1, Key:C Parent: B\n" +
-//                "Color: 1, Key:F Parent: D\n" +
-//                "Color: 1, Key:E Parent: F\n" +
-//                "Color: 0, Key:H Parent: F\n" +
-//                "Color: 1, Key:G Parent: H\n" +
-//                "Color: 1, Key:I Parent: H\n" +
-//                "Color: 0, Key:J Parent: I\n";
-//
-//        System.out.println(makeStringDetails(rbt));
-//
-//        boolean result = rbt.lookup("J");
-//        if (result) {
-//            System.out.println("found");
-//        } else {
-//            System.out.println("not found");
-//        }
+        System.out.println("makeString(rbt)" + makeString(rbt));
+        System.out.println("makeStringDetails(rbt)" + makeStringDetails(rbt));
 
-        // create RedBlackTree instance
-        RedBlackTree redBlackTree = new RedBlackTree();
-        double start = currentTimeMillis();
-        String inputLine;
-
-        // ********************************  create dictionary ********************************
-        try {
-            // read each line and add to the tree
-            URL oracle = new URL("http://www.math.sjsu.edu/~foster/dictionary.txt");
-            BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
-
-            while ((inputLine = in.readLine()) != null) {
-                redBlackTree.insert(inputLine);
-            }
-            in.close();
-
-        } catch (IOException e) {
-            System.out.println("MalformedURLException");
-            e.printStackTrace();
-        }
-
-        double end = currentTimeMillis();
-        double duration = end - start;
-        System.out.println("The duration of creating the dictionary is " + duration);
-
-        // ******************************** check with dictionary ********************************
-        String st;
-        String[] words;
-        RedBlackTree.Node node;
-        boolean isFound = false;
-        double startLooking = currentTimeMillis();
-        // check the word by calling lookup();
-        File file = new File("/Users/sijiagao/IdeaProjects/sjsu.Gao.cs146.project3/src/myPoem");
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            while ((st = br.readLine()) != null) {
-                words = st.split("\\s+");
-//                words = {"aaaa", "aaaaa"};
-                for (String s : words) {
-                    if(redBlackTree.lookup(s)){
-                        isFound = true;
-                    }
-                    else
-                        isFound = false;
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("FileNotFoundException");
-            e.printStackTrace();
-        }
-
-        double endLooking = currentTimeMillis();
-        double durationLooking = endLooking - startLooking;
-        if (isFound) {
-            System.out.println("The word is found");
+        boolean result = rbt.lookup("J");
+        if (result) {
+            System.out.println("found");
         } else {
-            System.out.println("The word is not found");
+            System.out.println("not found");
         }
-        System.out.println("The duration to check the spell is " + durationLooking);
+
+//        // create RedBlackTree instance
+//        RedBlackTree redBlackTree = new RedBlackTree();
+//        double start = currentTimeMillis();
+//        String inputLine;
+//
+//        // ********************************  create dictionary ********************************
+//        long currentTime = System.currentTimeMillis();
+//        File file = new File("/Users/sijiagao/IdeaProjects/RedBlackTreeProject-master/dictionary.txt");
+//
+//        try {
+//            // read each line and add to the tree
+//
+////            while ((inputLine = in.readLine()) != null) {
+////                redBlackTree.insert(inputLine);
+////            }
+////            in.close();
+//            Scanner scanner = new Scanner(file);
+//            while (scanner.hasNext()) {
+//                String string = scanner.nextLine();
+//                redBlackTree.insert(string);
+//            }
+//            long endingTime = System.currentTimeMillis();
+//            scanner.close();
+//
+//        } catch (FileNotFoundException e) {
+//            System.out.println("MalformedURLException");
+//            e.printStackTrace();
+//        }
+//
+//        double end = currentTimeMillis();
+//        double duration = end - start;
+//        System.out.println("The duration of creating the dictionary is " + duration);
+//
+//        // ******************************** check with dictionary ********************************
+//        String st;
+//        String[] words;
+//        RedBlackTree.Node node;
+//        boolean isFound = false;
+//        double startLooking = currentTimeMillis();
+//        // check the word by calling lookup();
+//        File file2 = new File("/Users/sijiagao/IdeaProjects/sjsu.Gao.cs146.project3/src/myPoem");
+//        try {
+//            BufferedReader br = new BufferedReader(new FileReader(file2));
+//            while ((st = br.readLine()) != null) {
+//                words = st.split("\\s+");
+////                words = {"aaaa", "aaaaa"};
+//                for (String s : words) {
+//                    if(redBlackTree.lookup(s)){
+//                        isFound = true;
+//                    }
+//                    else
+//                        isFound = false;
+//                }
+//            }
+//        } catch (IOException e) {
+//            System.out.println("FileNotFoundException");
+//            e.printStackTrace();
+//        }
+//
+//        double endLooking = currentTimeMillis();
+//        double durationLooking = endLooking - startLooking;
+//        if (isFound) {
+//            System.out.println("The word is found");
+//        } else {
+//            System.out.println("The word is not found");
+//        }
+//        System.out.println("The duration to check the spell is " + durationLooking);
     }
 
     public static String makeString (RedBlackTree t)
@@ -119,7 +120,7 @@ public class Tester {
         t.preOrderVisit(v);
         return v.result;
     }
-
+//
     public static String makeStringDetails (RedBlackTree redBlackTree){
         {
             class MyVisitor implements RedBlackTree.Visitor {
@@ -144,4 +145,80 @@ public class Tester {
     }
 
 }
-
+//
+//public class Tester {
+//    public static void main(String[] args) {
+//
+//    File file = new File("/Users/sijiagao/IdeaProjects/sjsu.Gao.cs146.project3/src/dictionary.txt");
+//    RedBlackTree dictionary = new RedBlackTree();
+//    try {
+//        Scanner scanner = new Scanner(file);
+//        long currentTime = System.currentTimeMillis();
+//        while (scanner.hasNext()) {
+//            String string = scanner.nextLine();
+//
+//            System.out.println(string);
+//            dictionary.insert(string);
+//        }
+//                System.out.println(makeStringDetails(dictionary));
+//        long endingTime = System.currentTimeMillis();
+//        scanner.close();
+//        int totalTime = (int) (endingTime - currentTime);
+//        System.out.println("The total time to insert is: " + totalTime + " ms");
+//
+//        currentTime = System.currentTimeMillis();
+//        boolean searchResult = dictionary.lookup("aaaa");
+//        endingTime = System.currentTimeMillis();
+//        long totalTimeMs = (endingTime - currentTime);
+//        if (searchResult) {
+//            System.out.println("The time to look up a string in Dictionary " + totalTimeMs + " ms");
+//            System.out.println("That's some fast search right there! :)");
+//        } else {
+//            System.out.println("Your word is not in our dictionary!");
+//            System.out.println("Time for the unsuccessful search... " + totalTimeMs + " ms");
+//        }
+//        //the line below can be uncommented to check the preOrderVisit
+//        //dictionary.preOrderVisit();
+//    } catch (FileNotFoundException e) {
+//        e.printStackTrace();
+//    }
+//
+//}
+//    public static String makeString (RedBlackTree t)
+//    {
+//        class MyVisitor implements RedBlackTree.Visitor {
+//            String result = "";
+//
+//            public void visit(RedBlackTree.Node n) {
+//                result = result + n.key;
+//            }
+//        }
+//        ;
+//        MyVisitor v = new MyVisitor();
+//        t.preOrderVisit(v);
+//        return v.result;
+//    }
+//    //
+//    public static String makeStringDetails (RedBlackTree redBlackTree){
+//        {
+//            class MyVisitor implements RedBlackTree.Visitor {
+//                String result = "";
+//
+//                public void visit(RedBlackTree.Node n) {
+//                    if (!(n.key).equals(""))
+//                        if (n.parent == null) {
+//                            result = result + "Color: " + n.color + ", Key:" + n.key + " Parent: " + "\n";
+//                        } else {
+//                            result = result + "Color: " + n.color + ", Key:" + n.key + " Parent: " + n.parent.key + "\n";
+//                        }
+//
+//                }
+//            }
+//            ;
+//            MyVisitor v = new MyVisitor();
+//            redBlackTree.preOrderVisit(v);
+//
+//            return v.result;
+//        }
+//    }
+//}
